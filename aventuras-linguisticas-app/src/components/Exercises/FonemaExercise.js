ecbcb985b5c07bead074eaa1d71c40a213b6cc07
Anahi Vasquez EPN <anahi.vasquez@epn.edu.ec>
2024-06-03 22:04:18 -0500

    Fix FonemaExercise
    detección de imágenes con Firebase storage

--- aventuras-linguisticas-app/src/components/Exercises/FonemaExercise.js
+++ aventuras-linguisticas-app/src/components/Exercises/FonemaExercise.js
@@ -1,8 +1,9 @@
-import React from 'react';  
+import React, { useState, useEffect } from 'react';  
 import { useParams, useNavigate } from 'react-router-dom';  
 import { useDocumentData } from 'react-firebase-hooks/firestore';  
 import { db } from "../../firebase";  // Importa tu instancia de Firestore
-import { collection, doc } from 'firebase/firestore';    
+import { collection, doc } from 'firebase/firestore';  
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 function FonemaExercise() {  
   const { fonema } = useParams();  
@@ -10,6 +11,27 @@ function FonemaExercise() {
   const [value, loading, error] = useDocumentData(  
     doc(collection(db, 'exercises'), fonema)  
   );  
+
+  const [imageURL, setImageURL] = useState(null);
+  const [imageLoading, setImageLoading] = useState(true);
+
+  useEffect(() => {
+    const storage = getStorage();
+    const imageRef = ref(storage, `images/Fonema_${fonema.toUpperCase()}.webp`);
+
+    console.log(`Fetching image for Fonema_${fonema.toUpperCase()}.webp`);
+
+    getDownloadURL(imageRef)
+      .then((url) => {
+        console.log("Image URL fetched:", url);
+        setImageURL(url);
+        setImageLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error al obtener la URL de descarga", error);
+        setImageLoading(false);
+      });
+  }, [fonema]);
 
   const handleNextPage = () => {
     navigate(`/PhonemicExerciseFull/${fonema}`); // Asegúrate de que la ruta sea correcta
@@ -22,7 +44,13 @@ function FonemaExercise() {
       {value && (  
         <div>  
           <h1>Fonema de la {fonema.toUpperCase()}</h1>  
-          <img src={value.image} alt="" />  
+          {imageLoading ? (
+            <p>Cargando imagen...</p>
+          ) : imageURL ? (
+            <img src={imageURL} alt={`Imagen del fonema ${fonema}`} />
+          ) : (
+            <p>No se pudo cargar la imagen.</p>
+          )}
           <audio src={value.audio} controls />  
           <button onClick={() => alert('Visto!')}>Visto</button>  
           <button onClick={() => alert('X!')}>X</button>  
@@ -33,4 +61,4 @@ function FonemaExercise() {
   );  
 }  
   
-export default FonemaExercise;  
+export default FonemaExercise;
